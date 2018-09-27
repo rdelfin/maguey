02bfa34eac71b1ecef1cff448c879fb3088e05ce
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-09-27 21:28:23 +0100

    Added uniform class

---
+++ include/maguey/uniform.hpp
@@ -0,0 +1,63 @@
+// Copyright 2018 Ricardo Delfin
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#pragma once
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+namespace maguey {
+
+enum UniformType {
+    UNIFORM_FLOAT,
+    UNIFORM_INT,
+    UNIFORM_FVEC_1,
+    UNIFORM_FVEC_2,
+    UNIFORM_FVEC_3,
+    UNIFORM_FVEC_4,
+    UNIFORM_IVEC_1,
+    UNIFORM_IVEC_2,
+    UNIFORM_IVEC_3,
+    UNIFORM_IVEC_4,
+    UNIFORM_UVEC_1,
+    UNIFORM_UVEC_2,
+    UNIFORM_UVEC_3,
+    UNIFORM_UVEC_4,
+    UNIFORM_FMAT_2,
+    UNIFORM_FMAT_3,
+    UNIFORM_FMAT_4,
+};
+
+
+class Uniform {
+ public:
+    Uniform();
+    Uniform(void* data, GLint program_id, UniformType type,
+            const std::string& unifrom_name);
+
+    void enable();
+    void update_data_ptr(void* new_data);
+
+    virtual ~Uniform();
+
+ private:
+    GLint loc;
+    void* data;
+    UniformType type;
+};
+
+}  // namespace maguey
---
+++ src/uniform.cpp
@@ -0,0 +1,148 @@
+// Copyright 2018 Ricardo Delfin
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#include<maguey/uniform.hpp>
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+namespace maguey {
+
+std::unordered_map<UniformType, std::function<void(GLint, void*)>>
+    uniform_func_map = {
+    {
+        UNIFORM_FLOAT,
+        [](GLint loc, void* data) {
+            glUniform1f(loc, *reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_INT,
+        [](GLint loc, void* data) {
+            glUniform1i(loc, *reinterpret_cast<int*>(data));
+        },
+    },
+    {
+        UNIFORM_FVEC_1,
+        [](GLint loc, void* data) {
+            glUniform1fv(loc, 1, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_FVEC_2,
+        [](GLint loc, void* data) {
+            glUniform2fv(loc, 1, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_FVEC_2,
+        [](GLint loc, void* data) {
+            glUniform2fv(loc, 1, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_FVEC_3,
+        [](GLint loc, void* data) {
+            glUniform3fv(loc, 1, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_IVEC_1,
+        [](GLint loc, void* data) {
+            glUniform1iv(loc, 1, reinterpret_cast<int*>(data));
+        },
+    },
+    {
+        UNIFORM_IVEC_2,
+        [](GLint loc, void* data) {
+            glUniform2iv(loc, 1, reinterpret_cast<int*>(data));
+        },
+    },
+    {
+        UNIFORM_IVEC_3,
+        [](GLint loc, void* data) {
+            glUniform3iv(loc, 1, reinterpret_cast<int*>(data));
+        },
+    },
+    {
+        UNIFORM_IVEC_4,
+        [](GLint loc, void* data) {
+            glUniform4iv(loc, 1, reinterpret_cast<int*>(data));
+        },
+    },
+    {
+        UNIFORM_UVEC_1,
+        [](GLint loc, void* data) {
+            glUniform1uiv(loc, 1, reinterpret_cast<unsigned*>(data));
+        },
+    },
+    {
+        UNIFORM_UVEC_2,
+        [](GLint loc, void* data) {
+            glUniform2uiv(loc, 1, reinterpret_cast<unsigned*>(data));
+        },
+    },
+    {
+        UNIFORM_UVEC_3,
+        [](GLint loc, void* data) {
+            glUniform3uiv(loc, 1, reinterpret_cast<unsigned*>(data));
+        },
+    },
+    {
+        UNIFORM_UVEC_4,
+        [](GLint loc, void* data) {
+            glUniform4uiv(loc, 1, reinterpret_cast<unsigned*>(data));
+        },
+    },
+    {
+        UNIFORM_FMAT_2,
+        [](GLint loc, void* data) {
+            glUniformMatrix2fv(loc, 1, false, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_FMAT_3,
+        [](GLint loc, void* data) {
+            glUniformMatrix3fv(loc, 1, false, reinterpret_cast<float*>(data));
+        },
+    },
+    {
+        UNIFORM_FMAT_4,
+        [](GLint loc, void* data) {
+            glUniformMatrix4fv(loc, 1, false, reinterpret_cast<float*>(data));
+        },
+    },
+};
+
+Uniform::Uniform() {
+}
+
+Uniform::Uniform(void* data, GLint program_id, UniformType type,
+                 const std::string& uniform_name)
+                 : data(data), type(type) {
+    this->loc = glGetUniformLocation(program_id, uniform_name.c_str());
+}
+
+void Uniform::enable() {
+    uniform_func_map[this->type](this->loc, this->data);
+}
+
+void Uniform::update_data_ptr(void* new_data) {
+    this->data = new_data;
+}
+
+Uniform::~Uniform() {
+}
+
+
+}  // namespace maguey
