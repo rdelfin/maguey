95b8e30b081c3c29db40af2e4e1c32028f14f797
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-10-06 15:30:34 -0400

    Moved mesh example to triangle example

--- examples/triangle_example.cpp
+++ examples/triangle_example.cpp
@@ -16,117 +16,68 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 
+#include <iostream>
 #include <vector>
 
+#include <glm/glm.hpp>
 #include <maguey/maguey.hpp>
+#include <maguey/internal/debug_util.hpp>
 
-
-const int SCREEN_WIDTH  = 1280;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
 
 const char VERT_SHADER[] = R"zzz(
-#version 330 core
-layout(location = 0) in vec3 pos_model;
-layout(location = 1) in vec3 vert_color;
-out vec3 frag_color;
-void main(){
-    gl_Position.xyz = pos_model;
-    gl_Position.w = 1.0;
-    frag_color = vert_color;
+#version 400 core
+layout(location = 0) in vec3 pos;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main() {
+    gl_Position = projection * view * vec4(pos, 1.0f);
 }
 )zzz";
 
 const char FRAG_SHADER[] = R"zzz(
-#version 330 core
-in vec3 frag_color;
+#version 400 core
 out vec3 color;
-void main(){
-  color = frag_color;
+void main() {
+    color = vec3(1.0, 1.0, 0.0);
 }
 )zzz";
 
-static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-    0.0f,  1.0f, 0.0f,
+static std::vector<glm::vec3> points = {
+    glm::vec3(1.0f,  -1.0f, 0.0f),
+    glm::vec3(-1.0f, -1.0f, 0.0f),
+    glm::vec3(0.0f,   1.0f, 0.0f),
 };
-
-static const GLfloat g_color_buffer_data[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f,
-};
-
 
 class MainGame : public maguey::Game {
  public:
-    MainGame() : maguey::Game("Display test", SCREEN_WIDTH, SCREEN_HEIGHT),
-        program(VERT_SHADER, FRAG_SHADER, {}, false) { }
-    virtual ~MainGame() {}
+    MainGame() : maguey::Game("Mesh test", 800, 600) { }
+
+    virtual ~MainGame() { }
 
  protected:
     void load(maguey::Camera* camera) override {
-        GLuint VertexArrayID;
-        glGenVertexArrays(1, &VertexArrayID);
-        glBindVertexArray(VertexArrayID);
-
-
-        // Generate 1 buffer, put the resulting identifier in vertexbuffer
-        glGenBuffers(1, &vertexbuffer);
-        // The following commands will talk about our 'vertexbuffer' buffer
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        // Give our vertices to OpenGL.
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
-                     g_vertex_buffer_data, GL_STATIC_DRAW);
-
-        glGenBuffers(1, &colorbuffer);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data),
-                     g_color_buffer_data, GL_STATIC_DRAW);
-
-        this->program.load();
+        this->mesh.load(points, maguey::Program(
+            std::string(VERT_SHADER), std::string(FRAG_SHADER),
+            {
+                camera->createProjectionMatrixUniform(),
+                camera->createViewMatrixUniform(),
+            }, false));
     }
 
     void update(maguey::Camera* camera) override { }
 
     void render(maguey::Camera* camera) override {
-        this->program.enable();
-
-       // 1st attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-            0,                  // attribute 0. No particular reason for 0, but
-                                // must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            nullptr);           // array buffer offset
-
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glVertexAttribPointer(
-            1,                  // attribute 1. No particular reason for 1, but
-                                // must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            nullptr);           // array buffer offset
-
-        // Draw the triangle !
-        // Starting from vertex 0; 3 vertices total -> 1 triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
+        this->mesh.render();
     }
 
  private:
-    maguey::Program program;
-    GLuint vertexbuffer;
-    GLuint colorbuffer;
+    maguey::TriangleMesh mesh;
 };
+
 
 int main() {
     MainGame game;
